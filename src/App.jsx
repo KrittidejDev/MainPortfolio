ea5f2f8c0df13038e410a095b0a9d1644400a857
KrittidejDev <Krittidej.Dev@gmail.com>
2024-09-24 00:40:05 +0700

    edite app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import ButtonNeon from "./components/ButtonNeon/buttonNeon";
+/* import ButtonNeon from "./components/ButtonNeon/buttonNeon"; */
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
-import SocialIcon from "./components/SocialIconHover/SocialIcon";
+/* import SocialIcon from "./components/SocialIconHover/SocialIcon"; */
 import "./index.css";
 
 function App() {
